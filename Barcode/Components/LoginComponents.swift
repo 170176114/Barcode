6f8179a55569024bb49492f23e6f30de63e8ddd0
170176114 <vtc170176114@gmail.com>
2021-09-12 14:23:38 +0800

    setup Login Page

--- Barcode/Components/LoginComponents.swift
+++ Barcode/Components/LoginComponents.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    static let FirstColor = Color("blue")
+    static let SecondColor = Color("orange")
+    static let ThirdColor = Color("red")
+    static let LastColor = Color("beige")
+}
