064b602038638b76c56a3e7924655cc81f2dd921
170176114 <vtc170176114@gmail.com>
2021-09-12 14:12:04 +0800

    Create Project

---
+++ Barcode/BarcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeApp.swift
+//  Barcode
+//
+//  Created by Pang on 12/9/2021.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
